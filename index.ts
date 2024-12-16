cae23f89fe5d8a97169d703b7a57dbb4117b4316
matiasbermudez <mabgrc@hotmail.com>
2024-12-16 10:25:03 -0300

    Middleware

--- index.ts
+++ index.ts
@@ -9,7 +9,19 @@ const crypto = require('crypto');
 dotenv.config();
 const PORT = process.env.PORT;
 const JWT_PRIVATE_KEY = process.env.JWT_PRIVATE_KEY;
-let jwtGlobalToken = "";
+
+//MIDDLEWARE EXPRESS
+function authMiddleware  (req,res,next){
+    try {       
+        const jwtHeadersRes = req.headers.authorization.split(' ')[1];
+            const data = jwt.verify(jwtHeadersRes , JWT_PRIVATE_KEY);
+            req._user = data;
+            next();
+    } catch (error) {
+        
+        res.status(401).json(error)
+    }
+}
 
 
 (async () => {
@@ -62,7 +74,6 @@ let jwtGlobalToken = "";
                             JWT_PRIVATE_KEY, 
                             { algorithm: 'HS256'}
                         );
-                        jwtGlobalToken = jwToken;
                         res.status(200).json(jwToken)
                     }else{
                         res.status(400).json('Usuario o Contraseña no encontrados')
@@ -72,27 +83,14 @@ let jwtGlobalToken = "";
             }
         });
 
-        app.get('/me', async (req,res)=>{
-            try {       
-                const jwtHeadersRes = req.headers.authorization.split(' ')[1];
-                console.log(jwtHeadersRes);
-                console.log(jwtGlobalToken)
-                if(jwtGlobalToken === jwtHeadersRes ){
-                    const data = jwt.verify(jwtHeadersRes , JWT_PRIVATE_KEY);
-                    const userData = await User.findOne({where : { id : data.user_id}});
-                    res.json({
-                        info : "user data ",
-                        userData
-                    })
-                }
-                else{
-                    const data = jwt.verify(jwtHeadersRes , "14214124");
-                    res.json(data)
-                }
-            } catch (error) {
-                
-                res.status(401).json(error)
-            }
+    
+        app.get('/me',authMiddleware,async (req,res)=>{
+            console.log(req._user)
+            const userData = await User.findOne({where : {id : req._user.user_id}});
+            res.json({
+                autorizado : "Token ok ",
+                userData
+            })
             
         });
 
